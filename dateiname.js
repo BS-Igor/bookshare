35c342fc329cf48e8151aa237d42bb3b870c5031
Igor Beierbach <igor.beierbach@gmail.com>
2020-06-28 14:48:55 +0200

    Cards von index nach dateiname.js verlagert und dateiname.js eingebunden

---
+++ dateiname.js
@@ -0,0 +1,15 @@
+
+var divTag
+var books = [
+{title:"Lorem ipsum", text:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel"},
+{title:"Lorem ipsum", text:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel"},
+{title:"Lorem ipsum", text:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel"}
+];
+
+for (var i = 0; i < books.length; i++) {
+  divTag = document.createElement('div');
+  divTag.appendChild(document.createTextNode(books[i].title));
+  divTag.appendChild(document.createTextNode(books[i].text));
+  document.getElementById('flex-cards').appendChild(divTag);
+}
+
