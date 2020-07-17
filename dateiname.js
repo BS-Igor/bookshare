0e4388ece9139dd3b032164fdf0f0fa932e8864f
Igor Beierbach <igor.beierbach@gmail.com>
2020-07-17 10:48:59 +0200

    API Abfrage, error , generischen Code erstellt

--- dateiname.js
+++ dateiname.js
@@ -1,15 +1,52 @@
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var xmlHttp = new XMLHttpRequest();
+var url = "https://ghibliapi.herokuapp.com/films";
 
-var divTag
-var books = [
-{title:"Lorem ipsum", text:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel"},
-{title:"Lorem ipsum", text:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel"},
-{title:"Lorem ipsum", text:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel"}
-];
+// Open a new connection, using the GET request on the URL endpoint
+xmlHttp.open("GET", url, true);
 
-for (var i = 0; i < books.length; i++) {
-  divTag = document.createElement('div');
-  divTag.appendChild(document.createTextNode(books[i].title));
-  divTag.appendChild(document.createTextNode(books[i].text));
-  document.getElementById('flex-cards').appendChild(divTag);
-}
+xmlHttp.onload = function () {
+// Begin accessing JSON data here
+    var data = JSON.parse(this.response);
+
+    if(xmlHttp.status >= 200 && xmlHttp.status < 400){
+   
+    data.forEach(filmchen => {
+
+/*  <div>
+      <h1>title</h1>
+      <p>description</p>
+    </div>    */
+
+      const content = document.getElementById('flex-cards');
+      // Create a div with a card class
+      const container = document.createElement('div');
+      content.appendChild(container);
+
+      // Create an h1 and set the text content to the film's title
+      const title = document.createElement('h1');
+      title.textContent = filmchen.title;
+      container.appendChild(title);
+
+      // Create a p and set the text content to the film's description
+      const paragraph = document.createElement('p');
+      filmchen.description = filmchen.description.substring(0, 300); // Limit to 300 chars
+      paragraph.textContent = filmchen.description;
+      paragraph.textContent = `${filmchen.description}...`; // End with an ellipses ...
+     
+      container.appendChild(paragraph);
+
+      console.log(filmchen.title);
+    });
+
+  } else{
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
+  }
+
+};
+
+// Send request
+xmlHttp.send();
 
