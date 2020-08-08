f104eeddf07f9e7be06d1d013afe01f91f7768c7
Igor Beierbach <igor.beierbach@gmail.com>
2020-08-08 12:33:40 +0200

    XHR error handling

--- dateiname.js
+++ dateiname.js
@@ -1,32 +1,54 @@
 // Create a request variable and assign a new XMLHttpRequest object to it.
-var xmlHttp = new XMLHttpRequest();
-var url = "https://ghibliapi.herokuapp.com/films";
 
-// Open a new connection, using the GET request on the URL endpoint
-xmlHttp.open("GET", url, true);
-var data = [];
-console.log(data);
 
-xmlHttp.onload = function () {
-  // Begin accessing JSON data here
 
-  if (xmlHttp.status >= 200 && xmlHttp.status < 400) {
-    data = JSON.parse(this.response);
-    //Javascript for each loop
-    generateCards(data);
-  } else {
-    const errorMessage = document.createElement('p');
-    errorMessage.textContent = `Gah, it's not working!`;
-    content.appendChild(errorMessage);
-  }
 
-};
+window.setTimeout(requestXmlHttp, 20);
+var data;
+var isSuccess;
 
-// Quelle: https://developer.mozilla.org/en-US/docs/Web/API/Event/type
- document.getElementById('search-input').addEventListener('keyup', filterData, false);
+function handleEvent(event) {
+  console.log(event.type);
+  document.getElementById('flex-cards').innerHTML = "<p>Verbindung zum Server verloren. Bitte versuchen Sie es später erneut.</p>";
+isSuccess = false;  
+}
+
+function addListeners(xhr) {
+  xhr.addEventListener('error', handleEvent);
+  xhr.addEventListener('abort', handleEvent);
+}
+
+function requestXmlHttp() {
+  var xmlHttp = new XMLHttpRequest();
+  var url = "https://ghiblinpi.herokuapp.com/films";
+  //addListeners(xmlHttp);
+  // Open a new connection, using the GET request on the URL endpoint
+  xmlHttp.open("GET", url, true);
+  xmlHttp.send();
+ // xmlHttp.status;
+
+if (xmlHttp.status >= 200 && xmlHttp.status < 400) {
+    isSuccess = true;
+    // Begin accessing JSON data here
+      data = JSON.parse(this.response);
+      generateCards(data);
+}  else {
+  document.getElementById('flex-cards').innerHTML = "<p>Verbindung zum Server verloren. Bitte versuchen Sie es später erneut.</p>";
+  isSuccess = false;
+}
+
+}
 
 function filterData() {
-  //entweder wie drunter oder so: document.getElementById('search-input'). value;
+  if(isSuccess){
+    filter();
+  } else {
+    console.log("isSuccess? " + isSuccess);
+  }
+}
+
+function filter() {
+  //entweder wie drunter oder so: document.getElementById('search-input').value;
   const input = document.querySelector('input').value;
 
   var dataFiltered = [];
@@ -36,39 +58,38 @@ function filterData() {
     if (element.title.toLowerCase().includes(input.toLowerCase()) || element.description.toLowerCase().includes(input.toLowerCase())) {
       // Quelle: https://www.freecodecamp.org/news/javascript-array-of-objects-tutorial-how-to-create-update-and-loop-through-objects-using-js-array-methods/
       dataFiltered.push(element);
-    } 
+    }
   });
-
-  if (dataFiltered.length === 0) {
-    document.getElementById('flex-cards').innerHTML = "<p>No result.</p>";
-  } else {
-    document.getElementById('flex-cards').innerHTML = "";
-  }
   generateCards(dataFiltered);
 }
 
-function generateCards(cards){   
-  cards.forEach(card => {
-    const content = document.getElementById('flex-cards');
-    // Create a div with a card class
-    const container = document.createElement('div');
-    content.appendChild(container);
+function generateCards(cards) {
+  document.getElementById('flex-cards').innerHTML = "";
+  if (cards.length === 0) {
+    document.getElementById('flex-cards').innerHTML = "<p>No result.</p>";
+  } else {
 
-    // Create an h1 and set the text content to the film's title
-    const title = document.createElement('h1');
-    title.textContent = card.title;
-    container.appendChild(title);
+    cards.forEach(card => {
+      const content = document.getElementById('flex-cards');
+      // Create a div with a card class
+      const container = document.createElement('div');
+      content.appendChild(container);
 
-    // Create a p and set the text content to the film's description
-    const paragraph = document.createElement('p');
-    card.description = card.description.substring(0, 300); // Limit to 300 chars
-    paragraph.textContent = card.description;
-    paragraph.textContent = `${card.description}…`; // End with an ellipses ...
+      // Create an h1 and set the text content to the film's title
+      const title = document.createElement('h1');
+      title.textContent = card.title;
+      container.appendChild(title);
 
-    container.appendChild(paragraph);
+      // Create a p and set the text content to the film's description
+      const paragraph = document.createElement('p');
+      card.description = card.description.substring(0, 300); // Limit to 300 chars
+      paragraph.textContent = card.description;
+      paragraph.textContent = `${card.description}…`; // End with an ellipses ...
 
-  });
+      container.appendChild(paragraph);
 
+    });
+  }
 }
 
 // function filterItems(arr, query) {
@@ -80,4 +101,3 @@ function generateCards(cards){
 // }
 
 // Send request
-xmlHttp.send();
